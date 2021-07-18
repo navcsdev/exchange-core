77a870e7c2bda9b49845f2d0960feb8fbb6324b2
navcsdev <nguyenanhvu.cs@gmail.com>
2021-07-18 23:43:58 +0700

    Init commit

---
+++ src/main/java/com/example/exchange/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.exchange.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/greeting")
+public class HelloWorldController {
+
+    @GetMapping
+    public Mono<String> hello() {
+        return Mono.just("Hello world");
+    }
+}
